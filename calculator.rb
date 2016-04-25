a020d44d66acead87cb19e38ac7c1a1e21ecd968
Kenneth Olson <kenneth.ko.olson@gmail.com>
2016-04-24 23:37:42 -0700

    add count to bottom

--- calculator.rb
+++ calculator.rb
@@ -1,30 +1,52 @@
 def calculator(int1, oper, int2)
 	if oper == "+"
-	int1 + int2
+	puts "#{int1} + #{int2} = #{int1 + int2}"
 	elsif oper == "-"
-	int1 - int2
+	puts "#{int1} - #{int2} = #{int1 - int2}"
 	elsif oper == "*"
-	int1 * int2
+	puts "#{int1} * #{int2} = #{int1 * int2}"
 	elsif oper == "/"
-	int1 / int2
+	puts "#{int1} / #{int2} = #{int1 / int2}"
 	else
 	puts "not a valid operator"
 end
 end
 
 #p calculator(12,"5", 3)		
+count = 0
+valid_input = false
+until valid_input
+
+
 puts "What would you like to calculate?"
 calc = gets.chomp
 
-int1 = calc[0].to_i
-int2 = calc[2].to_i
+	int1 = calc[0].to_i
+	int2 = calc[2].to_i
 
-if calc[1] == "+"
-	puts calculator(int1,"+",int2)
-elsif calc[1] == "-"
-	puts calculator(int1,"-",int2)
-elsif calc[1] == "*"
-	puts calculator(int1,"*",int2)
-elsif calc[1] == "/"
-	puts calculator(int1,"/",int2)	
+	if calc[1] == "+"
+		puts calculator(int1,"+",int2)
+		count = count + 1
+	elsif calc[1] == "-"
+		puts calculator(int1,"-",int2)
+		count = count + 1
+	elsif calc[1] == "*"
+		puts calculator(int1,"*",int2)
+		count = count + 1
+	elsif calc[1] == "/"
+		puts calculator(int1,"/",int2)
+		count = count + 1
+	end
+if calc == "done"	
+	valid_input = true
 end
+end
+	
+puts "you have performed #{count} operations"	
+	
+	
+
+	
+	
+	
+	
